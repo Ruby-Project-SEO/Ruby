f2e5b37c6cd41d50224be867722ca9bbb21d3e50
Laali Nembot <laalinembot@gmail.com>
2026-07-17 22:27:25 +0000

    Added user login page, working on functionality

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+      app.run(debug=True, host="0.0.0.0")
