7270ff6b35f679dfb9b1275279b3962bcce2f4a1
Laali Nembot <laalinembot@gmail.com>
2026-07-19 15:23:55 +0000

    Made changes to functionality of login.html and added welcome page

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def login():
     return render_template('login.html')
+
+@app.route('/entry', methods= ["GET", "POST"])
+def entry():
+    if request.method == "POST":
+       user = request.form["usr"]
+       return render_template('welcome.html', username= user )
+    else:
+        return render_template('login.html')
+
+@app.route('/recipe')
+def recipe():
+    return render_template('recipe.html')
+
 
 
 if __name__ == '__main__':
