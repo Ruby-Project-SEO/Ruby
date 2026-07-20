b096056c9da02c93b7cad5b671f1b813765d32a0
Christopher Hernandez <10schris2005@gmail.com>
2026-07-20 04:49:51 +0000

    i think finished gemini and api stuff

--- ruby.py
+++ ruby.py
@@ -15,6 +15,7 @@ client = genai.Client(api_key=my_api_key)
 foodurl = "https://api.spoonacular.com/food/products/search"
 drugurl = "https://api.fda.gov/drug/label.json"
 cosmeticurl = "https://world.openbeautyfacts.org/cgi/search.pl"
+engine = db.create_engine('sqlite:///item_status.db')
 
 
 def search_food(food):
@@ -58,11 +59,13 @@ def get_food_price(food_id):
   
 
 def search_drug(drug):
-  response = requests.get(drugurl,
-                            params={"search": f"openfda.brand_name:{drug}"
+    response = requests.get(drugurl,
+                            params={"search": f"openfda.brand_name:{drug}",
                                     "limit": 1})
 
     drug_status = response.json()
+
+    price = generate_price(drug)
 
     if "results" not in drug_status:
         return None
@@ -73,7 +76,8 @@ def search_drug(drug):
 
     for item in items:
       results.append({
-          "Drug": item.get("openfda, {}").get("brand_name", ["N/A"])[0],
+          "Drug": item.get("openfda", {}).get("brand_name", ["N/A"])[0],
+          "Price": price
       })
 
     return results
@@ -84,10 +88,12 @@ def search_drug(drug):
 def search_cosmetics(cosmetic):
   response = requests.get(cosmeticurl,
                           params={"search_terms": cosmetic,
-                                  "json:" 1,
+                                  "json": 1,
                                   "page_size": 1})
   
   cosmetic_status = response.json()
+
+  price = generate_price(cosmetic)
 
   if "products" not in cosmetic_status:
     return None
@@ -98,7 +104,8 @@ def search_cosmetics(cosmetic):
 
   for item in items:
     results.append({
-      "Cosmetic": item.get("product_name", "N/A")
+      "Cosmetic": item.get("product_name", "N/A"),
+      "Price": price
     })
 
   return results
@@ -106,7 +113,7 @@ def search_cosmetics(cosmetic):
 
 def select_item(item, table, key):
 
-    food_stats = pd.DataFrame([item])
+    item_status = pd.DataFrame([item])
 
     with engine.connect() as connection:
 
@@ -115,7 +122,7 @@ def select_item(item, table, key):
                             {"val": item[key]})
         connection.commit()
 
-    food_stats.to_sql('food_status', con=engine,
+    item_status.to_sql(table, con=engine,
                             if_exists='append', index=False)
 
 
@@ -167,4 +174,94 @@ def delete_saved(table, row_num):
 def get_link(item):
   query = item.replace(" ", "+")
   return f"https://www.google.com/search?q=buy+{query}"
+
+
+
+def generate_food_remedies(issue):
+  #prompt for gemini, modify it here
+  prompt = f"""
+  Given an issue, generate the 10 best foods in a comma separated list to help the person with that issue.
+  Issue: {issue}
+  """
+
+  resp = client.models.generate_content(
+      model="gemini-2.5-flash",
+      contents=prompt
+  )
+
+  foods = resp.text.split(",")
+  foods = [food.strip() for food in foods]
+
+  all_results = []
+
+  for food in foods:
+    result = search_food(food)
+    if result:
+      all_results.extend(result)
+  
+  return all_results
+
+def generate_drug_remedies(issue):
+  #prompt for gemini, modify it here
+  prompt = f"""
+  Given an issue, generate the 10 best drugs in a comma separated list to help the person with that issue.
+  Issue: {issue}
+  """
+
+  resp = client.models.generate_content(
+      model="gemini-2.5-flash",
+      contents=prompt
+  )
+  drugs = resp.text.split(",")
+  drugs = [drug.strip() for drug in drugs]
+
+  all_results = []
+
+  for drug in drugs:
+    result = search_drug(drug)
+    if result:
+      all_results.extend(result)
+  
+  return all_results
+
+def generate_cosmetic_remedy(issue):
+  #prompt for gemini, modify it here
+  prompt = f"""
+  Given an issue, generate the 10 best cosmetics in a comma separated list to help the person with that issue.
+  Don't give any explanation or anything else besides the comma separated list.
+  Issue: {issue}
+  """
+
+  resp = client.models.generate_content(
+      model="gemini-2.5-flash",
+      contents=prompt
+  )
+  cosmetics = resp.text.split(",")
+  cosmetics = [cosmetic.strip() for cosmetic in cosmetics]
+
+  all_results = []
+
+  for cosmetic in cosmetics:
+    result = search_cosmetics(cosmetic)
+    if result:
+      all_results.extend(result)
+  
+  return all_results
+
+
+def generate_price(item):
+  prompt = f"""
+  Given an item, generate the most common price this item would be listed at in USD.
+  Don't give any explanation or any other words other than just the price excluding the "$" symbol.
+  Item: {item}
+  """
+
+  resp = client.models.generate_content(
+      model="gemini-2.5-flash",
+      contents=prompt
+  )
+  try:
+    return float(resp.text.strip())
+  except ValueError:
+    return "N/A"
 
