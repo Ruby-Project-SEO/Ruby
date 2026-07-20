78145b400e8b5a34a4568cd26b98438cda873761
hhiamte <157152785+hhiamte@users.noreply.github.com>
2026-07-20 16:12:17 +0100

    initial dashboard functionality w/ routing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, session
 from flask_behind_proxy import FlaskBehindProxy
 
 app = Flask(__name__)
@@ -6,16 +6,19 @@ proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = 'c29bcfa698752666def85f68880d22d8'
 
 @app.route('/')
+def home():
+    return render_template('welcome.html', username=session.get('username'))
+
+@app.route('/login')
 def login():
     return render_template('login.html')
 
 @app.route('/entry', methods= ["GET", "POST"])
 def entry():
     if request.method == "POST":
-       user = request.form["usr"]
-       return render_template('welcome.html', username= user )
-    else:
-        return render_template('login.html')
+        session['username'] = request.form["usr"]
+        return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
 @app.route('/recipe')
 def recipe():
