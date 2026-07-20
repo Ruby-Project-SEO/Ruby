0bc2cede5eea2d58e52508d120510096bf24b98d
Angel Rodriguez <angeldr2002@gmail.com>
2026-07-20 21:16:16 +0000

    Removed stuff from app.py

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_behind_proxy import FlaskBehindProxy
+from dotenv import load_dotenv
+import os
+import requests
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = 'c29bcfa698752666def85f68880d22d8'
 
+SPOONACULAR_API_KEY = os.getenv("SPOONACULAR_API_KEY")
+
 @app.route('/')
 def login():
     return render_template('login.html')
 
-@app.route('/recipe')
+@app.route('/recipe', methods=['GET', 'POST'])
 def recipe():
     return render_template('recipe.html')
 
